aaec24902426678c03da5caa6753dabd003bb3dd
xuhuizhen <420981870@qq.com>
2019-01-25 15:39:04 +0800

    创建数据库xiaofeiniu.sql

---
+++ xiaofeiniu.sql
@@ -0,0 +1,15 @@
+#设置客户端语言
+SET NAMES UTF8;
+#放弃数据库(如果存在)xiaofeiniu
+DROP DATABASE IF EXISTS xiaofeiniu;
+#创建数据库xiaofeiniu
+CREATE DATABASE xiaofeiniu CHARSET=UTF8;
+#进入数据库
+USE xiaofeiniu;
+
+
+
+
+
+
+
